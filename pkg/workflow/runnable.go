9b3ec9b8ccbb9bf22a576ced486add3fb6bcc5c5
longsolong <0x19911221@gmail.com>
2019-10-25 21:59:19 +0800

    rm status

--- pkg/workflow/runnable.go
+++ pkg/workflow/runnable.go
@@ -5,22 +5,21 @@ import (
 	"reflect"
 )
 
+// Runnable ...
 type Runnable interface {
 	Run(ctx Context) (Return, error)
-
-	Stop() error
-
-	Status() string
+	Stop(ctx Context) error
 }
 
+// Return ...
 type Return struct {
 	State byte  // STATE_ const
 	Exit  int64 // Unix exit code
 	Error error // Go error
 }
 
+// GenRunnableType ...
 func GenRunnableType(r Runnable) string {
 	e := reflect.TypeOf(r).Elem()
 	return fmt.Sprintf("%s.%s", e.PkgPath(), e.Name())
 }
-
--- pkg/workflow/step/command/command.go
+++ pkg/workflow/step/command/command.go
@@ -1,11 +1,11 @@
 package command
 
 import (
-	"fmt"
+	"os/exec"
+
 	"github.com/longsolong/flow/pkg/workflow"
 	"github.com/longsolong/flow/pkg/workflow/state"
 	"github.com/longsolong/flow/pkg/workflow/step"
-	"os/exec"
 )
 
 // ShellCommand is a step that runs a single shell command with arguments.
@@ -13,15 +13,10 @@ type ShellCommand struct {
 	step.Step
 
 	Cmd *exec.Cmd
-
-	status string
 }
 
+// Run a shell
 func (s *ShellCommand) Run(ctx workflow.Context) (workflow.Return, error) {
-	// Set status before and after
-	s.SetStatus(fmt.Sprintf("runnning %v", s.Cmd.Args))
-	defer s.SetStatus(fmt.Sprintf("done running %v", s.Cmd.Args))
-
 	// Run the cmd and wait for it to return
 	exit := int64(0)
 	err := s.Cmd.Run()
@@ -39,14 +34,7 @@ func (s *ShellCommand) Run(ctx workflow.Context) (workflow.Return, error) {
 	return ret, nil
 }
 
-func (s *ShellCommand) Stop() error {
+// Stop run
+func (s *ShellCommand) Stop(ctx workflow.Context) error {
 	return nil
-}
-
-func (s *ShellCommand) Status() string {
-	return s.status
-}
-
-func (s *ShellCommand) SetStatus(msg string) {
-	s.status = msg
 }
